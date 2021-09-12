e1317ddb923cc0dd129888beec3d37c9ddd560d6
Daniil <892971@gmail.com>
2021-09-12 20:19:40 +0200

    task

--- task3.py
+++ task3.py
@@ -5,10 +5,10 @@ result_dict = {}
 with open('users.csv', 'r', encoding='utf-8') as f1, \
         open('hobby.csv', 'r', encoding='utf-8') as f2:
     user = (el for el in f1.read().splitlines())
-    hobbys = (el for el in f2.read().splitlines())
-    for hobbys, users in zip(hobbys, user):
-        result_dict[users.strip()] = hobbys.strip()
-    for el in hobbys:
+    hobby = (el for el in f2.read().splitlines())
+    for hobby, users in zip(hobby, user):
+        result_dict[users.strip()] = hobby.strip()
+    for el in hobby:
         sys.exit(1)
     for users in user:
         result_dict[user.strip()] = None
